f9859623d125ed6510b921d9399a43ebfb55b3e9
JBeanCode <js02bean@gmail.com>
2026-02-20 03:07:15 +0000

    josh-homePage-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,51 @@
 import { Link } from "react-router-dom";
+import xplorrlogo1 from "../assets/img/xplorrlogo 1.jpg";
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="topNavbar">
+      <nav className="navbar navbarCustom navbar-expand-lg d-flex">
+        <div className="container-fluid">
+          <a className="navbar-brand navHomeButton" href="#">
+            <img className="xplorrLogo" src={xplorrlogo1} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse ms-auto" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import RedwoodsImageUrl from "../assets/img/redwoods.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  return (
+    <div className="text-center d-flex flex-column align-items-center">
+      {/* <h1>Hello Rigo!!</h1> */}
+      <div className="row m-auto w-100 h-100">
+        <img src={RedwoodsImageUrl} />
+      </div>
+    </div>
+  );
+};
